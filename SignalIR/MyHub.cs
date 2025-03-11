214f17e1a38bf1d1ad4bf63ec9eee70030ba27cd
AA <Student@CAB4-2>
2025-03-11 16:30:24 +1000

    dd

--- SignalIR/MyHub.cs
+++ SignalIR/MyHub.cs
@@ -1,17 +1,29 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
-    internal class MyHub : Hub
-    {
+internal class MyHub : Hub
+{
     static Dictionary<string, ISingleClientProxy> clientsByNick = new();
+    private readonly Rooms rooms;
     public override Task OnConnectedAsync()
     {
         Clients.Caller.SendAsync("hello", "Придумайте ник");
         return base.OnConnectedAsync();
     }
-    public MyHub() 
-        { 
     
+    public void Nickname(string nick)
+    {
+        var check = clientsByNick.Keys.FirstOrDefault(s => s == nick);
+        if (check != null)
+        {
+            Clients.Caller.SendAsync("hello", "Этот ник занят. Придумайте другой");
+            return;
+        }
+        else
+        {
+            clientsByNick.Add(nick, Clients.Caller);
+            
         }
     }
+}
 
--- SignalIR/Rooms.cs
+++ SignalIR/Rooms.cs
@@ -1,4 +1,20 @@
 ﻿internal class Rooms
 {
+    string last = string.Empty;
 
+    internal void AddNewClient(string nick)
+    {
+        if (string.IsNullOrEmpty(last))
+            last = nick;
+        else
+        {
+            StartNewGame(last, nick);
+            last = string.Empty;
+        }
+    }
+
+    internal void StartNewGame()
+    {
+
+    }
 }
